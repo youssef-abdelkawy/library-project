354dc7202f865c8ac07f19202efc655181b9731f
Abdelkawy <154913276+youssef-abdelkawy@users.noreply.github.com>
2026-05-11 10:21:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@db:5432/librarydb'
+    # السطر ده هو "الجوكر" اللي بيخلي الموقع يشم رابط الداتابيز في ريلواي أوتوماتيك
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
+    # تصحيح بسيط لأن ريلواي بيبدأ بـ postgres والكود محتاج postgresql
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'secret_key_here'
